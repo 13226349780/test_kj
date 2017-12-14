45ca2cc2767d71bbc262d64d4e6cf69cc889fafa
lqf <746832476@qq.com>
2017-12-14 15:30:23 +0800

    12.14.参考http://blog.csdn.net/huilan_same/article/details/76572466

--- test/test_baidu.py
+++ test/test_baidu.py
@@ -7,12 +7,14 @@ from selenium.webdriver.common.by import By
 import unittest
 
 from log.log import logger
-from utils.config import Config
+from utils.config import Config, DATA_PATH
+from utils.file_reader import ExcelReader
 
 
 class TestBaiDu(unittest.TestCase):
 
     URL = Config().get('URL')
+    excel = DATA_PATH + '/baidu.xlsx'
     #URL = 'http://baidu.com'
     locator_result = (By.XPATH, '//div[contains(@class, "result")]/h3/a')
 
@@ -21,25 +23,21 @@ class TestBaiDu(unittest.TestCase):
         self.driver.get(self.URL)
     def tearDown(self):
         self.driver.quit()
-    def test_search_0(self):
-        self.driver.find_element_by_id('kw').send_keys('selenium 灰蓝')
-        self.driver.find_element_by_id('su').click()
-        time.sleep(2)
-        links = self.driver.find_elements_by_xpath('//div[contains(@class, "result")]/h3/a')
-        for link in links:
-            logger.info(link.text)
-    def test_search_1(self):
-        self.driver.find_element_by_id('kw').send_keys('Python selenium')
-        self.driver.find_element_by_id('su').click()
-        time.sleep(2)
-        #links = self.driver.find_elements(*self.locator_result)
-        links = self.driver.find_elements_by_xpath('//div[contains(@class, "result")]/h3/a')
-        for link in links:
-            logger.info(link.text)
-
+    def test_search(self):
+        datas = ExcelReader(self.excel).data
+        for d in datas:
+            with self.subTest(data = d):
+                self.sub_setUp()
+                self.driver.find_element_by_id('kw').send_keys(d['search'])
+                self.driver.find_element_by_id('su').click()
+                time.sleep(2)
+                links = self.driver.find_elements(*self.locator_result)
+                for link in links:
+                    logger.info(link.text)
+                self.sub_tearDown()
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
 
 
 
